5e05bae22ec066ac29cc11ac82eb5f48288fb439
Anthony Farquharson <anthony.farquharson@entelect.co.uk>
2024-01-02 11:40:31 +0000

    Adds server parameters

--- clients/mfe-two/vite.config.ts
+++ clients/mfe-two/vite.config.ts
@@ -1,20 +1,31 @@
 import federation from '@originjs/vite-plugin-federation';
 import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    federation({
-      name: 'mfe-two',
-      filename: 'remoteEntry.js',
-      exposes: {
-        './App': './src/App',
-        './Button': './src/button/Button',
-        './Remote': './src/Remote',
-      },
-      shared: ['react', 'react-dom', 'react-router', 'react-router-dom'],
-    }),
-  ],
-});
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd(), '') };
+
+  const SERVER_HOST = process.env.SERVER_HOST ?? '0.0.0.0';
+  const SERVER_PORT = parseInt(process.env.SERVER_PORT) ?? 5402;
+
+  return defineConfig({
+    server: {
+      host: SERVER_HOST,
+      port: SERVER_PORT,
+    },
+    plugins: [
+      react(),
+      federation({
+        name: 'mfe-two',
+        filename: 'remoteEntry.js',
+        exposes: {
+          './App': './src/App',
+          './Button': './src/button/Button',
+          './Remote': './src/Remote',
+        },
+        shared: ['react', 'react-dom', 'react-router', 'react-router-dom'],
+      }),
+    ],
+  });
+};
--- host/vite.config.ts
+++ host/vite.config.ts
@@ -1,18 +1,33 @@
 import federation from '@originjs/vite-plugin-federation';
 import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    federation({
-      name: 'host-app',
-      remotes: {
-        mfe_one: 'http://localhost:3501/assets/remoteEntry.js',
-        mfe_two: 'http://localhost:3502/assets/remoteEntry.js',
-      },
-      shared: ['react', 'react-dom', 'react-router', 'react-router-dom'],
-    }),
-  ],
-});
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd(), '') };
+
+  const SERVER_HOST = process.env.SERVER_HOST ?? '0.0.0.0';
+  const SERVER_PORT = parseInt(process.env.SERVER_PORT) ?? 5400;
+  const MFE_ONE_URL =
+    process.env.MFE_ONE_URL ?? 'http://localhost:5401/assets/remoteEntry.js';
+  const MFE_TWO_URL =
+    process.env.MFE_TWO_URL ?? 'http://localhost:5402/assets/remoteEntry.js';
+
+  return defineConfig({
+    server: {
+      host: SERVER_HOST,
+      port: SERVER_PORT,
+    },
+    plugins: [
+      react(),
+      federation({
+        name: 'host-app',
+        remotes: {
+          mfe_one: MFE_ONE_URL,
+          mfe_two: MFE_TWO_URL,
+        },
+        shared: ['react', 'react-dom', 'react-router', 'react-router-dom'],
+      }),
+    ],
+  });
+};
