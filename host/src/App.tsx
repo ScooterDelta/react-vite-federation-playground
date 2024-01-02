760b600eac4fb394942a1807361252db05ae45e5
Anthony Farquharson <anthony.farquharson@entelect.co.uk>
2024-01-02 14:15:16 +0000

    Removes unnecessary details

--- host/src/App.tsx
+++ host/src/App.tsx
@@ -1,25 +1,18 @@
 import MfeOneApp from 'mfe-one/app';
 import MfeTwoApp from 'mfe-two/app';
 import './App.css';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 
 function App() {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>Host</h1>
-      <div className="card">
-        <MfeOneApp />
-        <MfeTwoApp />
-      </div>
+      <p>
+        Content on the host is rendered by the host container, and will not be
+        visible on the MFE Client Applications unless loaded by the host
+      </p>
+
+      <MfeOneApp />
+      <MfeTwoApp />
     </>
   );
 }
