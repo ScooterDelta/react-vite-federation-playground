bc60087cbb11738fe7e64f8bbe5a80c78f67c6e5
Anthony Farquharson <anthony.farquharson@entelect.co.uk>
2024-01-02 15:42:03 +0000

    Adding react router links

--- host/src/App.tsx
+++ host/src/App.tsx
@@ -1,18 +1,9 @@
-import MfeOneApp from 'mfe-one/app';
-import MfeTwoApp from 'mfe-two/app';
 import './App.css';
 
 function App() {
   return (
     <>
       <h1>Host</h1>
-      <p>
-        Content on the host is rendered by the host container, and will not be
-        visible on the MFE Client Applications unless loaded by the host
-      </p>
-
-      <MfeOneApp />
-      <MfeTwoApp />
     </>
   );
 }
--- host/src/routes/root.tsx
+++ host/src/routes/root.tsx
@@ -1,3 +1,20 @@
 export const Root = () => {
-  return <>This route is on the Host Application</>;
+  return (
+    <div>
+      <h2>Host Application Root</h2>
+      <p>
+        Content on the host is rendered by the host container, and will not be
+        visible on the MFE Client Applications unless loaded by the host
+      </p>
+      <br />
+      <ul>
+        <li>
+          <a href="/mfe-one">MFE One</a>
+        </li>
+        <li>
+          <a href="/mfe-two">MFE Two</a>
+        </li>
+      </ul>
+    </div>
+  );
 };
