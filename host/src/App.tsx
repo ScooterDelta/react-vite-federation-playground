9697b9a9ad76f0d1a6d4c0bb6c6c96d9a1eeaf69
Anthony Farquharson <anthony.farquharson@entelect.co.uk>
2024-01-02 10:07:01 +0000

    Renaming host

--- host/src/App.tsx
+++ host/src/App.tsx
@@ -1,11 +1,8 @@
-import { useState } from 'react';
 import './App.css';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <div>
@@ -16,15 +13,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount(count => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
+      <h1>Host</h1>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
