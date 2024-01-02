4e2ed930d864e206adbe327370fc3625ee031d78
Anthony Farquharson <anthony.farquharson@entelect.co.uk>
2024-01-02 10:58:45 +0000

    Fixes remote

--- clients/mfe-two/src/Remote.tsx
+++ clients/mfe-two/src/Remote.tsx
@@ -3,7 +3,7 @@ import Button from './button/Button';
 export const Remote = () => {
   return (
     <>
-      <h2>This is the MFE One Button</h2>
+      <h2>This is the MFE Two Button</h2>
       <Button></Button>
     </>
   );
