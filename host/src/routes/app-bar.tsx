54f20dc070b652c4c145e925962b768664072dd7
Anthony Farquharson <anthony.farquharson@entelect.co.uk>
2024-01-03 11:43:38 +0000

    Adds heroicons

--- host/src/routes/app-bar.tsx
+++ host/src/routes/app-bar.tsx
@@ -1,3 +1,8 @@
+import {
+  PlayCircleIcon,
+  ServerIcon,
+  ServerStackIcon,
+} from '@heroicons/react/24/solid';
 import { Link, Outlet } from 'react-router-dom';
 
 export const AppBar = () => {
@@ -6,16 +11,22 @@ export const AppBar = () => {
       <div className="navbar bg-base-300">
         <div className="flex-1">
           <Link to="/" className="btn btn-ghost text-xl">
+            <PlayCircleIcon className="h-7 w-7 " />
             React Microfrontends Playground
           </Link>
         </div>
         <div className="flex-none">
           <ul className="menu menu-horizontal px-1">
             <li>
-              <Link to="/mfe-one">MFE One</Link>
+              <Link to="/mfe-one">
+                <ServerIcon className="h-6 w-6 text-blue-500" /> MFE One
+              </Link>
             </li>
             <li>
-              <Link to="/mfe-two">MFE Two</Link>
+              <Link to="/mfe-two">
+                <ServerStackIcon className="h-6 w-6 text-blue-500" />
+                MFE Two
+              </Link>
             </li>
           </ul>
         </div>
