843b17f40b8d59f58e69affbd05d541d4ed51564
Anthony Farquharson <anthony.farquharson@entelect.co.uk>
2024-01-04 11:40:02 +0000

    Updates styling on form 2

--- clients/mfe-one/src/routes/forms/second-form.tsx
+++ clients/mfe-one/src/routes/forms/second-form.tsx
@@ -14,35 +14,37 @@ export const SecondForm = () => {
   return (
     <div className="mfe1-h-full mfe1-bg-base-200">
       <form className="mfe1-h-full" onSubmit={handleSubmit(onSubmit)}>
-        <label className="mfe1-form-control mfe1-w-full mfe1-max-w-xs">
+        <label className="mfe1-form-control mfe1-w-full mfe1-max-w-xs mfe1-pl-6 mfe1-pt-4">
           <span className="mfe1-label-text">What is your favourite dog?</span>
           <input
             {...register('favouriteDog', { required: true, maxLength: 20 })}
             className="mfe1-input mfe1-input-bordered mfe1-w-full mfe1-max-w-xs"
           />
         </label>
-        <label className="mfe1-form-control mfe1-w-full mfe1-max-w-xs">
+        <label className="mfe1-form-control mfe1-w-full mfe1-max-w-xs mfe1-pl-6 mfe1-pt-4">
           <span className="mfe1-label-text">How many dogs do you have?</span>
           <input
             {...register('numberDogs', { min: 18, max: 99 })}
             className="mfe1-input mfe1-input-bordered mfe1-w-full mfe1-max-w-xs"
           />
         </label>
-        <label className="mfe1-form-control mfe1-w-full mfe1-max-w-xs">
+        <label className="mfe1-form-control mfe1-w-full mfe1-max-w-xs mfe1-pl-6 mfe1-pt-4">
           <span className="mfe1-label-text">What is your favourite cat?</span>
           <input
             {...register('favouriteCat', { pattern: /^[A-Za-z]+$/i })}
             className="mfe1-input mfe1-input-bordered mfe1-w-full mfe1-max-w-xs"
           />
         </label>
-        <label className="mfe1-form-control mfe1-w-full mfe1-max-w-xs">
+        <label className="mfe1-form-control mfe1-w-full mfe1-max-w-xs mfe1-pl-6 mfe1-pt-4">
           <span className="mfe1-label-text">How many cats do you have?</span>
           <input
             {...register('numberCats', { min: 18, max: 99 })}
             className="mfe1-input mfe1-input-bordered mfe1-w-full mfe1-max-w-xs"
           />
         </label>
-        <input type="submit" className="mfe1-btn mfe1-btn-primary" />
+        <div className="mfe1-py-4 mfe1-pl-6">
+          <input type="submit" className="mfe1-btn mfe1-btn-primary" />
+        </div>
       </form>
     </div>
   );
