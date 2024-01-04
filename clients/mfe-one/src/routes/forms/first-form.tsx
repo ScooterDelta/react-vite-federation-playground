1f06969999a729c7485b327418ad61eda6aa209d
Anthony Farquharson <anthony.farquharson@entelect.co.uk>
2024-01-04 11:35:20 +0000

    Adds form contents

--- clients/mfe-one/src/routes/forms/first-form.tsx
+++ clients/mfe-one/src/routes/forms/first-form.tsx
@@ -1,3 +1,62 @@
+import { SubmitHandler, useForm } from 'react-hook-form';
+
+interface IFormInput {
+  email: string;
+  password: string;
+}
+
 export const FirstForm = () => {
-  return <></>;
+  const { register, handleSubmit } = useForm<IFormInput>();
+  const onSubmit: SubmitHandler<IFormInput> = data => console.log(data);
+
+  return (
+    <div className="mfe1-hero mfe1-bg-base-200">
+      <div className="mfe1-hero-content mfe1-flex-col lg:mfe1-flex-row-reverse">
+        <div className="mfe1-text-center lg:mfe1-text-left">
+          <h1 className="mfe1-text-5xl mfe1-font-bold">Login now!</h1>
+          <p className="mfe1-py-6">
+            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
+            a id nisi.
+          </p>
+        </div>
+        <div className="mfe1-mfe1-card mfe1-shrink-0 mfe1-w-full mfe1-max-w-sm mfe1-shadow-2xl mfe1-bg-base-100">
+          <form className="mfe1-card-body" onSubmit={handleSubmit(onSubmit)}>
+            <div className="mfe1-form-control">
+              <label className="mfe1-label">
+                <span className="mfe1-label-text">Email</span>
+              </label>
+              <input
+                type="email"
+                placeholder="email"
+                className="mfe1-input mfe1-input-bordered"
+                {...register('email', { required: true, maxLength: 20 })}
+              />
+            </div>
+            <div className="mfe1-form-control">
+              <label className="mfe1-label">
+                <span className="mfe1-label-text">Password</span>
+              </label>
+              <input
+                type="password"
+                placeholder="password"
+                className="mfe1-input mfe1-input-bordered"
+                {...register('password', { required: true, maxLength: 20 })}
+              />
+              <label className="mfe1-label">
+                <a
+                  href="#"
+                  className="mfe1-label-text-alt mfe1-link mfe1-link-hover">
+                  Forgot password?
+                </a>
+              </label>
+            </div>
+            <div className="mfe1-form-control mfe1-mt-6">
+              <button className="mfe1-btn mfe1-btn-primary">Login</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
--- clients/mfe-one/src/routes/forms/second-form.tsx
+++ clients/mfe-one/src/routes/forms/second-form.tsx
@@ -1,3 +1,49 @@
+import { SubmitHandler, useForm } from 'react-hook-form';
+
+interface IFormInput {
+  favouriteDog: string;
+  favouriteCat: string;
+  numberDogs: number;
+  numberCats: number;
+}
+
 export const SecondForm = () => {
-  return <></>;
+  const { register, handleSubmit } = useForm<IFormInput>();
+  const onSubmit: SubmitHandler<IFormInput> = data => console.log(data);
+
+  return (
+    <div className="mfe1-h-full mfe1-bg-base-200">
+      <form className="mfe1-h-full" onSubmit={handleSubmit(onSubmit)}>
+        <label className="mfe1-form-control mfe1-w-full mfe1-max-w-xs">
+          <span className="mfe1-label-text">What is your favourite dog?</span>
+          <input
+            {...register('favouriteDog', { required: true, maxLength: 20 })}
+            className="mfe1-input mfe1-input-bordered mfe1-w-full mfe1-max-w-xs"
+          />
+        </label>
+        <label className="mfe1-form-control mfe1-w-full mfe1-max-w-xs">
+          <span className="mfe1-label-text">How many dogs do you have?</span>
+          <input
+            {...register('numberDogs', { min: 18, max: 99 })}
+            className="mfe1-input mfe1-input-bordered mfe1-w-full mfe1-max-w-xs"
+          />
+        </label>
+        <label className="mfe1-form-control mfe1-w-full mfe1-max-w-xs">
+          <span className="mfe1-label-text">What is your favourite cat?</span>
+          <input
+            {...register('favouriteCat', { pattern: /^[A-Za-z]+$/i })}
+            className="mfe1-input mfe1-input-bordered mfe1-w-full mfe1-max-w-xs"
+          />
+        </label>
+        <label className="mfe1-form-control mfe1-w-full mfe1-max-w-xs">
+          <span className="mfe1-label-text">How many cats do you have?</span>
+          <input
+            {...register('numberCats', { min: 18, max: 99 })}
+            className="mfe1-input mfe1-input-bordered mfe1-w-full mfe1-max-w-xs"
+          />
+        </label>
+        <input type="submit" className="mfe1-btn mfe1-btn-primary" />
+      </form>
+    </div>
+  );
 };
