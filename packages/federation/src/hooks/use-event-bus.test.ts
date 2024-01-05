aa0d76ba9953b2ddfb1bd2552cfc90066c43c046
Anthony Farquharson <anthony.farquharson@entelect.co.uk>
2024-01-05 14:22:46 +0000

    Adds jest tests to package

---
+++ packages/federation/src/hooks/use-event-bus.test.ts
@@ -0,0 +1,18 @@
+import { renderHook } from '@testing-library/react';
+import { act } from 'react-dom/test-utils';
+import { useEventBus } from './use-event-bus';
+
+describe('Testing event bus communication', () => {
+  test('Should fire event as expected', () => {
+    let event: string | undefined;
+    const callback = (callbackEvent: string) => (event = callbackEvent);
+
+    const { result } = renderHook(() => useEventBus('customEvent', callback));
+
+    expect(event).toBeUndefined();
+    act(() => {
+      result.current('my event');
+    });
+    expect(event).toEqual('my event');
+  });
+});
