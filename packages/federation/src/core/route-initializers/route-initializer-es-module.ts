38fe7512ac2b3cb771295dc17c4ec53d8d9fd4a6
Anthony Farquharson <anthony.farquharson@entelect.co.uk>
2024-01-08 15:59:03 +0000

    Updates esmodule import comments

--- packages/federation/src/core/route-initializers/route-initializer-es-module.ts
+++ packages/federation/src/core/route-initializers/route-initializer-es-module.ts
@@ -6,6 +6,10 @@ export const routeInitializerESModule: RouteInitializer =
     const qualifiedPrefix = shouldIncludePrefix
       ? `${microApplicationPrefix}/`
       : '';
+
+    // WARN - Failing since the import comes from a variable, currently dynamic imports must be coded at compile time
+    // - see issue: https://github.com/originjs/vite-plugin-federation/issues/401
+    // - see discussion: https://github.com/originjs/vite-plugin-federation/discussions/193
     const lazyModule = await import(`${qualifiedPrefix}${importPath}`);
     return { Component: lazyModule.default };
   };
