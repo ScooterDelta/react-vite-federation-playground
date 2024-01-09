76d0757c7eedbc67038223f15b4bb25d2bbc2ccf
Anthony Farquharson <anthony.farquharson@entelect.co.uk>
2024-01-09 13:42:41 +0000

    Refactors federation modules

--- host/src/federation-helpers/route-initializer-module-federation.ts
+++ host/src/federation-helpers/route-initializer-module-federation.ts
@@ -1,16 +1,19 @@
 import { RouteInitializer } from '@react-playground/federation';
+import {
+  __federation_method_getRemote,
+  __federation_method_setRemote,
+} from '__federation__';
 
 export const routeInitializerModuleFederation =
   (remoteEntryUrl: string): RouteInitializer =>
   (importPath, microApplicationPrefix) =>
   async () => {
-    const federation = await import('__federation__');
-    federation.__federation_method_setRemote(microApplicationPrefix!, {
+    __federation_method_setRemote(microApplicationPrefix!, {
       url: remoteEntryUrl,
       format: 'esm',
       from: 'vite',
     });
-    const module = await federation.__federation_method_getRemote(
+    const module = await __federation_method_getRemote(
       microApplicationPrefix!,
       `./${importPath}`
     );
